6df5252b73502021191c50109e1cee825b0ed658
Kevin Pinho Iamin <kiamin@1worldsync.com>
2021-07-16 21:59:06 -0300

    continuação do desenvolvimento.

--- forca.py
+++ forca.py
@@ -3,6 +3,22 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acerto = False
+
+    while not enforcou and not acerto:
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print("Encontrei a letra {} na posição {}".format(chute, index))
+            index = index + 1
+
+        print("jogando...")
+
     print("Fim do jogo")
 
 
